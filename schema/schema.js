46b37f5cb06768141f4983ecb9b4202588f9fbea
KateBilova <kateryna.bilova@extrawest.com>
2023-04-28 13:50:47 +0300

    setup; GQL schema; json-server;

---
+++ schema/schema.js
@@ -0,0 +1,42 @@
+const { default: axios } = require('axios');
+const graphql = require('graphql');
+// const _ = require('lodash');
+
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: {
+      type: GraphQLString,
+    },
+    firstName: {
+      type: GraphQLString,
+    },
+    age: {
+      type: GraphQLInt,
+    },
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        // can returns user object
+        // can return Promise
+        // return _.find(users, { id: args.id });
+        return axios
+          .get(`http://localhost:3000/users/${args.id}`)
+          .then((res) => res.data);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
