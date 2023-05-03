88d767b466dd42141c0192ce6304c6714d5ff620
KateBilova <kateryna.bilova@extrawest.com>
2023-05-03 15:57:25 +0300

    MongoDB, Apollo setup

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import SongList from './components/SongList';
+
+const client = new ApolloClient({});
+// assums that graphQL server is available on route "/graphql"
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <div>Lyrics</div>
+        <SongList />
+      </div>
+    </ApolloProvider>
+  );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector('#root'));
